4f3436e674669bac831e9d186ce8431344b515ef
Everton Martins <evertonmartins154@gmail.com>
2024-04-02 18:48:55 -0300

    Add entities

---
+++ src/main/java/com/martinseverton/passin/domain/attendee/Attendee.java
@@ -0,0 +1,42 @@
+package com.martinseverton.passin.domain.attendee;
+
+import com.martinseverton.passin.domain.event.Event;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter@Setter
+public class Attendee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
